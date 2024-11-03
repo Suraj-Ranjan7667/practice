6540b617fa0aaa2d9e07e3138d435499946bafd1
Suraj Ranjan <132946363+Suraj-Ranjan7667@users.noreply.github.com>
2024-11-03 16:25:53 +0530

    Delete backend directory

--- backend/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports.isAuthenticated = (req, res, next) => {
-    if (req.session && req.session.user && req.session.user.role === 'admin') {
-      next();
-    } else {
-      res.status(401).send('Unauthorized');
-    }
-  };
